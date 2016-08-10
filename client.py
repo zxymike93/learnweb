3a9d7dff18fb09ae53aec3cb82c425861faeaaf7
gayu-mike <zxymike93@gmail.com>
2016-08-10 11:55:57 +0800

    create 一个超小型的 client
    # 这个 client 连接本地的主机 '127.0.0.1' 端口为 2000
    # 发送请求 / 接收响应
    # 打印出 请求 / 响应 的信息

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+host = '127.0.0.1'
+port = 2000
+
+s = socket.socket()
+
+s.connect((host, port))
+print("连接IP地址'127.0.0.1:2000'")
+
+ip, port = s.getsockname()
+print('本机的 ip 和 port {} {}'.format(ip, port))
+
+http_request = 'GET / HTTP/1.1\r\nhost:{}\r\n\r\n'.format(host)
+request = http_request.encode('utf-8')
+s.send(request)
+print('发送"请求"', request)
+
+response = s.recv(1024)
+print('收到"响应"', response)
+print('把'响应'转为 str 格式', response.decode('utf-8'))

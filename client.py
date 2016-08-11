a04d2ca91276e3cac3ba2b4106764f8c2b6d355d
gayu-mike <zxymike93@gmail.com>
2016-08-11 14:22:34 +0800

    把向服务器发送 HTTP 请求并且获得数据这个过程封装成函数
    # 定义如下
    def get(url):
        '''
        返回的数据类型为 bytes
        '''
        pass
    
    
    """
    资料:
    在 Python3 中，bytes 和 str 的互相转换方式是
    str.encode('utf-8')
    bytes.decode('utf-8')
    
    send 函数的参数和 recv 函数的返回值都是 bytes 类型
    """
    
    
    # 使用
    def main():
        url = 'http://movie.douban.com/top250'
        r = get(url)
        print(r)
    
    
    if __name__ == '__main__':
        main()

--- client.py
+++ client.py
@@ -2,27 +2,42 @@
 
 import socket
 
-host = '127.0.0.1'
-port = 2000
-
 s = socket.socket()
 
-s.connect((host, port))
-print("连接IP地址'127.0.0.1:2000'")
 
-ip, port = s.getsockname()
-print('本机的 ip 和 port {} {}'.format(ip, port))
+def get(url):
+    pars = parsed_url(url)
+    protocol = pars[0]
+    host = pars[1]
+    port = pars[2]
+    path = pars[3]
+    
+    s.connect((host, port))
+    print("连接 url: ")
 
-http_request = 'GET / HTTP/1.1\r\nhost:{}\r\n\r\n'.format(host)
-request = http_request.encode('utf-8')
-s.send(request)
-print('发送"请求"', request)
+    ip, port = s.getsockname()
+    print('本机的 ip 和 port {} {}'.format(ip, port))
 
-response = b''
-while True:
-    r = s.recv(1024)
-    response += r
-    if len(r) == 0:
-      break
+    http_request = 'GET /{} {}/1.1\r\nhost:{}\r\nConnection: close\r\n\r\n'.format(path, protocol, host)
+    request = http_request.encode('utf-8')
+    s.send(request)
+    print('发送"请求"', request)
+
+    response = b''
+    while True:
+        r = s.recv(1024)
+        response += r
+        if len(r) == 0:
+          break
     # print('收到"响应"', response)
-print('把'响应'转为 str 格式', response.decode('utf-8'))
+    return('把'响应'转为 str 格式', response.decode('utf-8'))
+
+
+def main():
+    url = 'http://movie.douban.com/top250'
+    r = get(url)
+    print(r)
+
+
+if __name__ == '__main__':
+    main()

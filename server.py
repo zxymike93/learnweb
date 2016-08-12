c529b275696ab20322adcd78b8d7a995bcbfd8a3
gayu-mike <zxymike93@gmail.com>
2016-08-13 00:49:55 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,26 +2,64 @@
 
 import socket
 
-host = ''
-port = 2000
 
-s = socket.socket()
+def log(*args, **kwargs):
+    """
+    用 log 代替 print
+    """
+    pass
 
-s.bind((host, port))
 
-while True:
-    # listen() 开始监听
-    s.listen(7)
-    
-    # accept() 接收来自 client 的连接
-    connection, address = s.accept()
-    
-    request = connection.recv(1024)
-    print('接收来自 ip {} 的 "请求"\n{}'.format(address, request.decode('utf-8')))
-    
-    response = b'<h1>Hello World!</h1>'
-    connection.sendall(response)
-    print('向 client 发送"响应"', response）
-    
-    connection.close()
-    print('关闭连接')
+def error(code=404):
+    """
+    根据 code 返回不同的错误响应
+    目前只有 404
+    """
+    pass
+
+
+def response_for_path(path):
+    pass
+
+
+def run(host='', port=3000):
+    """
+    运行程序（即 启动服务器）
+    """
+    # 初始化 socket 的套路，先记住
+    # 使用 with 可以保证程序终端的时候正确关闭 socket 释放占用的端口
+    with socket.socket() as s:
+        s.bind((host, port))
+        while True:
+            # 监听/接收/读取 请求数据，编码成字符串
+            s.listen(3)
+            # accept() 接收来自 client 的连接
+            connection, address = s.accept()
+            r = connection.recv(1024)
+            r = r.decode('utf-8')
+            # log('ip and request, {}\n{}'.format(address, request))
+            try:
+                # 
+                # 
+                path = r.split()[1]
+                # 
+                request.method = r.split()[0]
+                # 
+                request.body = r.split('\r\n\r\n', 1)[1]
+                # 
+                response = resoponse_for_path(path)
+                # 
+                connection.sendall(response)
+            except Exception as e:
+                log('error', e)
+            # 处理完请求，关闭连接
+            connection.close()
+
+
+if __name__ == '__main__':
+    # 生成配置并且运行程序
+    config = dict(
+        host='',
+        port=3000,
+    )
+    run(**config)

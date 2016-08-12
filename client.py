68e71b00b78ed871f67b52ee3c0fe579ead9cac1
gayu-mike <zxymike93@gmail.com>
2016-08-12 13:10:51 +0800

    抓取’豆瓣TOP250‘网页信息
    def 四个空函数，负责处理复杂的逻辑：
    1. url 分析
    2. 针对不同 protocol 的 socket 实例创建
    3. 读取 response 信息
    4. 把 response 解析成 状态码/headers/body
    
    def get(url) 拿到 url 后运用上面的函数，向网站发送 GET请求，得到响应的 状态码/headers/body

--- client.py
+++ client.py
@@ -2,36 +2,74 @@
 
 import socket
 
-s = socket.socket()
+
+def parsed_url(url):
+    """
+    分析 url 
+    返回 protocol, host, port, path
+    """
+    pass
 
 
+def socket_by_protocol(protocol):
+    """
+    根据 protocol 是 HTTP / HTTPS
+    使用不同的 socket 初始化方式
+    返回一个 socket 实例
+    """
+    pass
+
+
+def response_by_socket(s):
+    """
+    参数是一个 socket 实例
+    返回这个 socket 读取（传输）的所有数据
+    也就是 response
+    """
+    pass
+
+
+def parsed_response(r):
+    """
+    把 response 解析出 status_code, headers, body
+    status_code 是 int
+    headers 是 dict
+    body 是 str
+    """
+    pass
+
+
+# 把上面复杂的逻辑全部封装成函数
 def get(url):
-    pars = parsed_url(url)
-    protocol = pars[0]
-    host = pars[1]
-    port = pars[2]
-    path = pars[3]
+    """
+    发送 GET请求 并返回 响应
+    包含 状态码 / headers / body
+    """
+    # 分析出 url 的 protocol, host, port, path
+    protocol, host, port, path = parsed_url(url)
     
+    # 创建 socket 实例
+    # 建立连接
+    s = socket_by_protocol(protocol)
     s.connect((host, port))
-    print("连接 url: ")
-
-    ip, port = s.getsockname()
-    print('本机的 ip 和 port {} {}'.format(ip, port))
-
-    http_request = 'GET /{} {}/1.1\r\nhost:{}\r\nConnection: close\r\n\r\n'.format(path, protocol, host)
-    request = http_request.encode('utf-8')
-    s.send(request)
-    print('发送"请求"', request)
-
-    response = b''
-    while True:
-        r = s.recv(1024)
-        response += r
-        if len(r) == 0:
-          break
-    # print('收到"响应"', response)
-    return('把'响应'转为 str 格式', response.decode('utf-8'))
-
+    
+    # request 以 utf-8 编码发送
+    request = 'GET {} HTTP/1.1\r\nhost:{}\r\nConnection: close\r\n\r\n'.format(path, host)
+    encoding = 'utf-8'
+    s.send(request.encode(encoding))
+    
+    # 接收 response 并解码为 str
+    response = response_by_socket(s)
+    r = response.decode(encoding)
+    
+    # 如果 status_code 是301
+    # 生成一个重定向
+    status_code, headers, body = parsed_response(r)
+    if status_code == 301:
+        url = headers['Location']
+        return get(url)
+    
+    return status_code, headers, body
 
 def main():
     url = 'http://movie.douban.com/top250'

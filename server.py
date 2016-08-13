c85996146aaeb517bff4c626957adf6adcfe1411
gayu-mike <zxymike93@gmail.com>
2016-08-13 13:18:27 +0800

    Update server.py

--- server.py
+++ server.py
@@ -10,6 +10,27 @@ def log(*args, **kwargs):
     print('log', *args, **kwargs)
 
 
+def route_index():
+    """
+    PATH  '/'
+    """
+    pass
+
+
+def route_image():
+    """
+    PATH  '/doge.gif'
+    """
+    pass
+
+
+def route_message():
+    """
+    PATH  '/messages'
+    """
+    pass
+
+
 def error(code=404):
     """
     根据 code 返回不同的错误响应
@@ -18,8 +39,27 @@ def error(code=404):
     pass
 
 
-def response_for_path(path):
+def parsed_path(path):
     pass
+
+
+def response_for_path(path):
+    """
+    根据 path 调用响应的处理函数
+    没有处理的 path 会返回 404
+    """
+    path, query = parsed_path(path)
+    request.path = path
+    request.query = query
+    log('path and query', path,query)
+    
+    responses = {
+        '/': route_index,
+        '/messages': route_message,
+        '/doge.gif': route_image,
+    }
+    response = responses.get(path, error)
+    return response()
 
 
 def run(host='', port=3000):
@@ -35,20 +75,21 @@ def run(host='', port=3000):
             s.listen(3)
             # accept() 接收来自 client 的连接
             connection, address = s.accept()
-            r = connection.recv(1024)
-            r = r.decode('utf-8')
-            # log('ip and request, {}\n{}'.format(address, request))
+            req = connection.recv(1024)
+            req = req.decode('utf-8')
+            # log('ip and request, {}\n{}'.format(address, req))
+            # try..except.. 排除会引起异常的请求
             try:
-                # 
-                # 
-                path = r.split()[1]
-                # 
+                # chrome 会发空请求导致 split 得到空 list
+                # 用 try 放置程序崩溃
+                path = req.split()[1]
+                # 设置 request 的 method
                 request.method = r.split()[0]
-                # 
+                # 把 body 放入 request 中
                 request.body = r.split('\r\n\r\n', 1)[1]
-                # 
+                # 用 response_for_path 函数来得到 path 对应的响应内容（页面）
                 response = resoponse_for_path(path)
-                # 
+                # Send response to Client
                 connection.sendall(response)
             except Exception as e:
                 log('error', e)

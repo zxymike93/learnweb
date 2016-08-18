ae28407d6ea0ac554306502e0d015c40db6529f8
gayu-mike <zxymike93@gmail.com>
2016-08-18 16:48:33 +0800

    8.18号暂用

---
+++ homework/818.py
@@ -0,0 +1,458 @@
+#! server.py
+import socket
+import urllib.parse
+
+from utils import log
+
+from routes import route_static
+from routes import route_dict
+
+
+# 定义一个 class 用于保存请求的数据
+class Request(object):
+    def __init__(self):
+        self.method = 'GET'
+        self.path = ''
+        self.query = {}
+        self.body = ''
+        self.headers = {}
+        self.cookies = {}
+
+    def add_cookies(self):
+        """
+        height=169; user=gua
+        :return:
+        """
+        cookies = self.headers.get('Cookie', '')
+        kvs = cookies.split('; ')
+        log('cookie', kvs)
+        for kv in kvs:
+            if '=' in kv:
+                k, v = kv.split('=')
+                self.cookies[k] = v
+
+    def add_headers(self, header):
+        """
+        Accept-Language: zh-CN,zh;q=0.8
+        Cookie: height=169; user=gua
+        """
+        # lines = header.split('\r\n')
+        lines = header
+        for line in lines:
+            k, v = line.split(': ', 1)
+            self.headers[k] = v
+        self.add_cookies()
+
+    def form(self):
+        body = urllib.parse.unquote(self.body)
+        args = body.split('&')
+        f = {}
+        for arg in args:
+            k, v = arg.split('=')
+            f[k] = v
+        return f
+
+
+#
+request = Request()
+
+
+def error(request, code=404):
+    """
+    根据 code 返回不同的错误响应
+    目前只有 404
+    """
+    # 之前上课我说过不要用数字来作为字典的 key
+    # 但是在 HTTP 协议中 code 都是数字似乎更方便所以打破了这个原则
+    e = {
+        404: b'HTTP/1.x 404 NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>',
+    }
+    return e.get(code, b'')
+
+
+def parsed_path(path):
+    """
+    message=hello&author=gua
+    {
+        'message': 'hello',
+        'author': 'gua',
+    }
+    """
+    index = path.find('?')
+    if index == -1:
+        return path, {}
+    else:
+        path, query_string = path.split('?', 1)
+        args = query_string.split('&')
+        query = {}
+        for arg in args:
+            k, v = arg.split('=')
+            query[k] = v
+        return path, query
+
+
+def response_for_path(path):
+    path, query = parsed_path(path)
+    request.path = path
+    request.query = query
+    log('path and query', path, query)
+    """
+    根据 path 调用相应的处理函数
+    没有处理的 path 会返回 404
+    """
+    r = {
+        '/static': route_static,
+        # '/': route_index,
+        # '/login': route_login,
+        # '/messages': route_message,
+    }
+    r.update(route_dict)
+    response = r.get(path, error)
+    return response(request)
+
+
+def run(host='', port=3000):
+    """
+    启动服务器
+    """
+    # 初始化 socket 套路
+    # 使用 with 可以保证程序中断的时候正确关闭 socket 释放占用的端口
+    log('start at', '{}:{}'.format(host, port))
+    with socket.socket() as s:
+        s.bind((host, port))
+        # 无限循环来处理请求
+        while True:
+            # 监听 接受 读取请求数据 解码成字符串
+            s.listen(3)
+            connection, address = s.accept()
+            r = connection.recv(1000)
+            r = r.decode('utf-8')
+            log('ip and request, {}\n{}'.format(address, r))
+            # 因为 chrome 会发送空请求导致 split 得到空 list
+            # 所以这里判断一下防止程序崩溃
+            if len(r.split()) < 2:
+                continue
+            path = r.split()[1]
+            # 设置 request 的 method
+            request.method = r.split()[0]
+            request.add_headers(r.split('\r\n\r\n', 1)[0].split('\r\n')[1:])
+            # 把 body 放入 request 中
+            request.body = r.split('\r\n\r\n', 1)[1]
+            # 用 response_for_path 函数来得到 path 对应的响应内容
+            response = response_for_path(path)
+            # 把响应发送给客户端
+            connection.sendall(response)
+            # 处理完请求, 关闭连接
+            connection.close()
+
+
+if __name__ == '__main__':
+    # 生成配置并且运行程序
+    config = dict(
+        host='',
+        port=3000,
+    )
+    # 如果不了解 **kwargs 的用法, 上过基础课的请复习函数, 新同学自行搜索
+    run(**config)
+
+
+
+#! models.py
+import json
+
+from utils import log
+
+
+def save(data, path):
+    """
+    data 是 dict 或者 list
+    path 是保存文件的路径
+    """
+    s = json.dumps(data, indent=2, ensure_ascii=False)
+    with open(path, 'w+', encoding='utf-8') as f:
+        # log('save', path, s, data)
+        f.write(s)
+
+
+def load(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        s = f.read()
+        # log('load', s)
+        return json.loads(s)
+
+
+class Model(object):
+    """
+    Model 是所有 model 的基类
+    @classmethod 是一个套路用法
+    例如
+    user = User()
+    user.db_path() 返回 User.txt
+    """
+    @classmethod
+    def db_path(cls):
+        """
+        cls 是类名, 谁调用的类名就是谁的
+        classmethod 有一个参数是 class(这里我们用 cls 这个名字)
+        所以我们可以得到 class 的名字
+        """
+        classname = cls.__name__
+        path = '{}.txt'.format(classname)
+        return path
+
+    @classmethod
+    def all(cls):
+        """
+        all 方法(类里面的函数叫方法)使用 load 函数得到所有的 models
+        """
+        path = cls.db_path()
+        models = load(path)
+        # 这里用了列表推导生成一个包含所有 实例 的 list
+        # m 是 dict, 用 cls(m) 可以初始化一个 cls 的实例
+        # 不明白就 log 大法看看这些都是啥
+        ms = [cls(m) for m in models]
+        return ms
+
+    @classmethod
+    def find_by(cls, **kwargs):
+        """
+        用法如下，kwargs 是只有一个元素的 dict
+        u = User.find_by(username='gua')
+        """
+        log('kwargs, ', kwargs)
+        k, v = '', ''
+        for key, value in kwargs.items():
+            k, v = key, value
+        all = cls.all()
+        for m in all:
+            if v == m.__dict__[k]:
+                return m
+        return None
+
+    def __repr__(self):
+        """
+        __repr__ 是一个魔法方法
+        简单来说, 它的作用是得到类的 字符串表达 形式
+        比如 print(u) 实际上是 print(u.__repr__())
+        """
+        classname = self.__class__.__name__
+        properties = ['{}: ({})'.format(k, v) for k, v in self.__dict__.items()]
+        s = '\n'.join(properties)
+        return '< {}\n{} >\n'.format(classname, s)
+
+    def save(self):
+        """
+        用 all 方法读取文件中的所有 model 并生成一个 list
+        把 self 添加进去并且保存进文件
+        """
+        log('debug save')
+        models = self.all()
+        log('models', models)
+        models.append(self)
+        l = [m.__dict__ for m in models]
+        path = self.db_path()
+        save(l, path)
+
+
+class User(Model):
+    """
+    User 是一个保存用户数据的 model
+    现在只有两个属性 username 和 password
+    """
+    def __init__(self, form):
+        self.username = form.get('username', '')
+        self.password = form.get('password', '')
+
+    def validate_login(self):
+        # return self.username == 'gua' and self.password == '123'
+        u = User.find_by(username=self.username)
+        return u is not None and u.password == self.password
+
+    def validate_register(self):
+        return len(self.username) > 2 and len(self.password) > 2
+
+
+class Message(Model):
+    """
+    Message 是用来保存留言的 model
+    """
+    def __init__(self, form):
+        self.author = form.get('author', '')
+        self.message = form.get('message', '')
+
+
+def test():
+    # users = User.all()
+    # u = User.find_by(username='gua')
+    # log('users', u)
+    form = dict(
+        username='gua',
+        password='gua',
+    )
+    u = User(form)
+    u.save()
+    u.save()
+    u.save()
+    u.save()
+    u.save()
+    u.save()
+
+if __name__ == '__main__':
+    test()
+
+
+
+#! routes.py
+from utils import log
+from models import Message
+from models import User
+
+import random
+
+
+# 这个函数用来保存所有的 messages
+message_list = []
+session = {}
+
+
+def random_str():
+    seed = 'abcdefjsad89234hdsfkljasdkjghigaksldf89weru'
+    s = ''
+    for i in range(16):
+        random_index = random.randint(0, len(seed) - 2)
+        s += seed[random_index]
+    return s
+
+
+def template(name):
+    """
+    根据名字读取 templates 文件夹里的一个文件并返回
+    """
+    path = 'templates/' + name
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def current_user(request):
+    session_id = request.cookies.get('user', '')
+    username = session.get(session_id, '游客')
+    return username
+
+
+def route_index(request):
+    """
+    主页的处理函数, 返回主页的响应
+    """
+    header = 'HTTP/1.x 210 VERY OK\r\nContent-Type: text/html\r\n'
+    body = template('index.html')
+    username = current_user(request)
+    body = body.replace('{{username}}', username)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def response_with_headers(headers):
+    """
+Content-Type: text/html
+Set-Cookie: user=gua
+    """
+    header = 'HTTP/1.x 210 VERY OK\r\n'
+    header += ''.join(['{}: {}\r\n'.format(k, v)
+                           for k, v in headers.items()])
+    return header
+
+
+def route_login(request):
+    """
+    登录页面的路由函数
+    """
+    headers = {
+        'Content-Type': 'text/html',
+        # 'Set-Cookie': 'height=169; gua=1; pwd=2; Path=/',
+    }
+    # log('login, headers', request.headers)
+    log('login, cookies', request.cookies)
+    username = current_user(request)
+    if request.method == 'POST':
+        form = request.form()
+        u = User(form)
+        if u.validate_login():
+            session_id = random_str()
+            session[session_id] = u.username
+            headers['Set-Cookie'] = 'user={}'.format(session_id)
+            result = '登录成功'
+        else:
+            result = '用户名或者密码错误'
+    else:
+        result = ''
+    body = template('login.html')
+    body = body.replace('{{result}}', result)
+    body = body.replace('{{username}}', username)
+    header = response_with_headers(headers)
+    r = header + '\r\n' + body
+    # log('login', r)
+    return r.encode(encoding='utf-8')
+
+
+def route_register(request):
+    """
+    注册页面的路由函数
+    """
+    header = 'HTTP/1.x 210 VERY OK\r\nContent-Type: text/html\r\n'
+    if request.method == 'POST':
+        form = request.form()
+        u = User(form)
+        if u.validate_register():
+            u.save()
+            result = '注册成功<br> <pre>{}</pre>'.format(User.all())
+        else:
+            result = '用户名或者密码长度必须大于2'
+    else:
+        result = ''
+    body = template('register.html')
+    body = body.replace('{{result}}', result)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_message(request):
+    """
+    消息页面的路由函数
+    """
+    log('本次请求的 method', request.method)
+    if request.method == 'POST':
+        form = request.form()
+        msg = Message(form)
+        log('post', form)
+        message_list.append(msg)
+        # 应该在这里保存 message_list
+    header = 'HTTP/1.x 200 OK\r\nContent-Type: text/html\r\n'
+    # body = '<h1>消息版</h1>'
+    body = template('html_basic.html')
+    msgs = '<br>'.join([str(m) for m in message_list])
+    body = body.replace('{{messages}}', msgs)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_static(request):
+    """
+    静态资源的处理函数, 读取图片并生成响应返回
+    """
+    filename = request.query.get('file', 'doge.gif')
+    path = 'static/' + filename
+    with open(path, 'rb') as f:
+        header = b'HTTP/1.x 200 OK\r\nContent-Type: image/gif\r\n\r\n'
+        img = header + f.read()
+        return img
+
+
+# 路由字典
+# key 是路由(路由就是 path)
+# value 是路由处理函数(就是响应)
+route_dict = {
+    '/': route_index,
+    '/login': route_login,
+    '/register': route_register,
+    '/messages': route_message,
+}

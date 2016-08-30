e8651fe0eeecba42352f90ad671000ea3f4f1402
zxymike <zxymike93@gmail.com>
2016-08-30 23:52:57 +0800

    修改微博的功能

--- routes_weibo.py
+++ routes_weibo.py
@@ -28,10 +28,11 @@ def route_weibo_index(request):
     # 手动处理 weibos 这个 list
     # 把每个 weibo 以 <p> 的形式展现在页面
     def weibo_tag(weibo):
-        return '<p>{} from {}@{} <a href="/weibo/delete?id={}">删除</a></p>'.format(
+        return '<p>{} from {}@{} <a href="/weibo/delete?id={}">删除</a> <a href="/weibo/edit?id={}">修改</a></p>'.format(
             weibo.content,
             user.username,
             weibo.created_time,
+            weibo.id,
             weibo.id,
         )
     # 用 join() 返回 str
@@ -43,14 +44,15 @@ def route_weibo_index(request):
 
 def route_weibo_new(request):
     """
-    返回微博表单的路由
+    发表新微博的页面
+    在 form 里面写上微博内容
+    post 的 action 则是 /weibo/add 这个路由
     """
     headers = {
         'Content-Type': 'text/html',
     }
     username = current_user(request)
     header = response_with_header(headers)
-    log('Debug username', username)
     user = User.find_by(username=username)
     body = template('weibo_new.html')
     r = header + '\r\n' + body
@@ -62,20 +64,15 @@ def route_weibo_add(request):
         'Content-Type': 'text/html',
     }
     username = current_user(request)
-    log('username', username)
+    log('发微博的用户: ', username)
     header = response_with_header(headers)
-    # log('add header', header)
     user = User.find_by(username=username)
-    # log('add user', user)
-    # 创建微博
+    # 创建一个新微博实例
+    # 就是把 weibo_new.html 的数据处理
     form = request.form()
-    # log('form', form)
     w = Weibo(form)
-    # log('w', w)
     w.user_id = user.id
-    # log('user_id', w.user_id)
     w.save()
-    # log('save', w)
     return redirect('/weibo?user_id={}'.format(user.id))
 
 
@@ -91,6 +88,40 @@ def route_weibo_delete(request):
     weibo_id = int(weibo_id)
     w = Weibo.find(weibo_id)
     w.delete()
+    return redirect('/weibo?user_id={}'.format(user.id))
+
+
+def route_weibo_edit(request):
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    header = response_with_header(headers)
+    # 这个 query.get 是在 weibo_index路由 里面放上去的
+    # 用来指定要修改那一条微博
+    weibo_id = request.query.get('id', -1)
+    weibo_id = int(weibo_id)
+    w = Weibo.find(weibo_id)
+    if w is None:
+        return error(request)
+    # 生成一个 edit 页面
+    body = template('weibo_edit.html',
+                    weibo_id=w.id,
+                    weibo_content=w.content)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_weibo_update(request):
+    username = current_user(request)
+    user = User.find_by(username=username)
+    form = request.form()
+    content = form.get('content', '')
+    weibo_id = int(form.get('id', -1))
+    w = Weibo.find(weibo_id)
+    if user.id != w.user_id:
+        return error(request)
+    w.content = content
+    w.save()
     return redirect('/weibo?user_id={}'.format(user.id))
 
 
@@ -112,4 +143,6 @@ route_dict = {
     '/weibo/new': login_required(route_weibo_new),
     '/weibo/add': login_required(route_weibo_add),
     '/weibo/delete': login_required(route_weibo_delete),
+    '/weibo/edit': login_required(route_weibo_edit),
+    '/weibo/update': login_required(route_weibo_update),
 }

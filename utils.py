719a307f7d9e98d9ece6b4961ee84eeb708aeb7e
zxymike <zxymike93@gmail.com>
2016-08-30 17:52:53 +0800

    增强的log: 显示log的时间

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import time
+
 def log(*args, **kwargs):
     """
     用 log 代替 print
+    time.time() 返回 Unix time
+    再转为常用时间格式
     """
-    print('log', *args, **kwargs)
+    format = '%Y/%m/%d %H:%M:%S'
+    value = time.localtime(int(time.time()))
+    dt = time.strftime(format, value)
+    print(dt, *args, **kwargs)
